8fcf8230d6bc017bcc1a32a9e1b7545eaf2e21b6
Alkarin <avaughan11@gmail.com>
2015-12-18 09:52:16 -0800

    Adding Funny

---
+++ FunnyPlant/src/FunnyPlant.java
@@ -0,0 +1,43 @@
+/**
+ * Created by alkarin on 11/25/15.
+ */
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FunnyPlant {
+    public static void main(String[] args) {
+        System.out.println("Enter how many people need to be fed and how many starting fruits");
+        String[] input = new Scanner(System.in).nextLine().split(" ");
+        Integer people = Integer.valueOf(input[0]);
+        Integer production = Integer.valueOf(input[1]);
+
+        List<Plant> plants = IntStream.rangeClosed(1, production).mapToObj(value -> new Plant()).collect(Collectors.toList());
+
+        while(production < people){
+            plants.forEach(Plant::addWeek);
+            production = plants.stream().mapToInt(Plant::getFruits).sum();
+            IntStream.rangeClosed(1, production).forEach(v -> plants.add(new Plant()));
+        }
+        System.out.println(plants.stream().mapToInt(Plant::getWeek).max().getAsInt());
+    }
+}
+class Plant{
+    private Integer week;
+    Plant(){
+        week = 1;
+    }
+    public void addWeek(){
+        week++;
+    }
+    public Integer getWeek(){
+        return week;
+    }
+    public Integer getFruits(){
+        return week - 1;
+    }
+}
